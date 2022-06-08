c21bae4946e47d9e493c535c7236eefb5b165ea2
viesha <vieshaac130@gmail.com>
2022-06-08 13:50:05 +0300

    Updated padoms and todos.js files with server address consts

--- frontend/src/services/todos.js
+++ frontend/src/services/todos.js
@@ -1,38 +1,39 @@
 import axios from 'axios';
-
+//const server_add = 'http://localhost:8000';      // addrese lokalai versijai
+const server_add = 'http://192.168.88.240:8000' // addrese produkcijas versijai
 class TodoDataService{
 
   getAll(token){      
     axios.defaults.headers.common["Authorization"] = "Token " + token;      
-    return axios.get('http://192.168.88.240:8000/api/todos/');
+    return axios.get(server_add +  '/api/todos/');
   }   
 
   createTodo(data, token){
     axios.defaults.headers.common["Authorization"] = "Token " + token;
-    return axios.post("http://192.168.88.240:8000/api/todos/", data);
+    return axios.post(server_add +  "/api/todos/", data);
   }
    
   updateTodo(id, data, token){
     axios.defaults.headers.common["Authorization"] = "Token " + token;
-    return axios.put(`http://192.168.88.240:8000/api/todos/${id}`, data);
+    return axios.put(server_add +  `/api/todos/${id}`, data);
   }
 
   deleteTodo(id, token){
     axios.defaults.headers.common["Authorization"] = "Token " + token;
-    return axios.delete(`http://192.168.88.240:8000/api/todos/${id}`);
+    return axios.delete(server_add +  `/api/todos/${id}`);
   }   
 
   completeTodo(id, token){
     axios.defaults.headers.common["Authorization"] = "Token " + token;
-    return axios.put(`http://192.168.88.240:8000/api/todos/${id}/complete`);    
+    return axios.put(server_add +  `/api/todos/${id}/complete`);    
   }   
 
   login(data){
-    return axios.post("http://192.168.88.240:8000/api/login/", data);
+    return axios.post(server_add + "/api/login/", data);
   }   
 
   signup(data){
-    return axios.post("http://192.168.88.240:8000/api/signup/", data);
+    return axios.post(server_add + "/api/signup/", data);
   }      
 }
 
